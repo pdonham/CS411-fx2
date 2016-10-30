f77986d2784fb0cd5cbdfa355ef8b09a97698cf6
perryd <perryd@bu.edu>
2016-10-30 11:25:43 -0400

    Base files for FX2
    NB: Run npm install in project directory to fetch module dependencies

---
+++ routes/fx2.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var router = express.Router();
+var request = require("request");
+
+router.get ('/', function (req, res, next) {
+
+    var options = { method: 'POST',
+        url: 'http://apilayer.net/api/live',
+        qs:
+        { access_key: '707d4d2111a1976c7c4bbd767a9bf3a6',
+            source: 'USD',
+            currencies: 'EUR',
+            format: '1' },
+        headers:
+        { 'content-type': 'application/x-www-form-urlencoded',
+            'postman-token': 'a1da4a62-ef9f-70be-f9d3-e024a72e32bf',
+            'cache-control': 'no-cache' } };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+
+var rate = JSON.parse(body).quotes.USDEUR;
+        var value = rate * 100;
+console.log(rate);
+        res.send({value: value});
+    });
+
+});
+
+router.post ('/', function (req, res, next) {
+    var options = { method: 'POST',
+        url: 'http://apilayer.net/api/live',
+        qs:
+        { access_key: 'register for key at apilayer.net',
+            source: 'USD',
+            currencies: 'EUR',
+            format: '1' },
+        headers:
+        { 'content-type': 'application/x-www-form-urlencoded',
+            'postman-token': 'a1da4a62-ef9f-70be-f9d3-e024a72e32bf',
+            'cache-control': 'no-cache' } };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+
+        var rate = JSON.parse(body).quotes.USDEUR;
+
+
+        var dollars = req.body.dollars;
+        var result = dollars * rate;
+        res.send({value: result});
+
+
+    });
+
+
+});
+
+module.exports = router;
